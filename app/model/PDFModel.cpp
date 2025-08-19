8e9a7a192f022af22c9584ea339e98d6de8c1975
Yexkr <3078961710@qq.com>
2025-08-19 18:21:48 +0800

    feat: add poppler and complete file opening

--- app/model/PDFModel.cpp
+++ app/model/PDFModel.cpp
@@ -11,18 +11,13 @@ bool PDFModel::openFromFile() {
     file.setNameFilter(tr("PDFDocument (*.pdf)"));
     file.setViewMode(QFileDialog::Detail);
     QString path = file.getOpenFileName();
-    // auto _document = Poppler::Document::load(path);
-    // if (_document)
-    // {
-    //     document = std::move(_document);
-    //     qDebug() << tr("打开成功");
-    //     return true;
-    // }
-    // qDebug() << tr("打开失败");
-    // return false;
-    if(!path.isEmpty()) {
-        qDebug() << path ;
-        filePath = path;
+    auto _document = Poppler::Document::load(path);
+    if (_document)
+    {
+        document = std::move(_document);
+        qDebug() << tr("打开成功") << path;
+        return true;
     }
-    return true;
+    qDebug() << tr("打开失败") << path;
+    return false;
 }
--- app/model/PDFModel.h
+++ app/model/PDFModel.h
@@ -3,14 +3,14 @@
 #include <QObject>
 #include <QString>
 #include <QFileDialog>
-// #include <poppler/qt6/poppler-qt6.h>
+#include <poppler/qt6/poppler-qt6.h>
 
 class PDFModel : public QObject
 {
     Q_OBJECT
 private:
     QString filePath;
-    // std::unique_ptr<Poppler::Document> document;
+    std::unique_ptr<Poppler::Document> document;
 public:
     PDFModel(){};
     ~PDFModel(){};
